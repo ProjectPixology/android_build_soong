a1f81fc2f45fc0b08aceb62125a3b5196d118eef
TriyPlus <triyplus@gmail.com>
2021-01-28 18:23:06 +0000

    dumpvars: replace displayed lineage values

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -139,7 +139,10 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 var BannerVars = []string{
 	"PLATFORM_VERSION_CODENAME",
 	"PLATFORM_VERSION",
-	"LINEAGE_VERSION",
+	"PIXOLOGY_CODENAME",
+	"PIXOLOGY_NUM",
+	"PIXOLOGY_VERSION",
+	"PIXOLOGY_BUILDTYPE",
 	"TARGET_PRODUCT",
 	"TARGET_BUILD_VARIANT",
 	"TARGET_BUILD_TYPE",
